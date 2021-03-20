f9f316cd3021396284d6b1fff08bd6ecee4d1f2d
Sanket Mohapatra <www.sanket1mohapatra@gmail.com>
2021-03-20 09:47:44 +0530

    fix

--- dist/index.js
+++ dist/index.js
@@ -17,7 +17,7 @@ const DB_name = 'db.json';
 const Collection_name = 'images';
 const upload_path = 'uploads';
 const upload = multer({ dest: `${upload_path}/` });
-const db = new Loki(`${upload_path}/${DB_name}`, { persistenceMethod: 'fs' });
+const db = new Loki(`${upload_path}/${DB_name}`, { persistenceMethod: 'fs' }); //Can use any db for this.
 const app = express();
 app.use(cors());
 const utils_1 = require("./utils");
@@ -26,9 +26,10 @@ app.post('/profile', upload.single('avatar'), (req, res) => __awaiter(void 0, vo
         const col = yield utils_1.loadCollection(Collection_name, db);
         const data = col.insert(req.file);
         db.saveDatabase();
-        res.send({ id: data.id, fileName: data.fileName, originalName: data.originalName });
+        res.send({ id: data.$loki, OriginalName: data.originalname, Filename: data.filename, status: "File uploaded successfully", statusNum: 1, });
     }
     catch (error) {
+        res.send({ status: "File upload not successful", statusNum: 0, errorMsg: error });
     }
 }));
 app.listen(3000, function () {
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const DB_name = 'db.json'
 const Collection_name = 'images'
 const upload_path = 'uploads'
 const upload = multer({ dest: `${upload_path}/` });
-const db = new Loki(`${upload_path}/${DB_name}`, { persistenceMethod: 'fs' })
+const db = new Loki(`${upload_path}/${DB_name}`, { persistenceMethod: 'fs' }) //Can use any db for this.
 
 const app = express()
 app.use(cors())
@@ -23,9 +23,9 @@ app.post('/profile', upload.single('avatar'), async (req, res) => {
         const data = col.insert(req.file)
 
         db.saveDatabase()
-        res.send({ id: data.id, fileName: data.fileName, originalName: data.originalName });
+        res.send({ id: data.$loki, OriginalName: data.originalname, Filename: data.filename, status: "File uploaded successfully", statusNum: 1, });
     } catch (error) {
-
+        res.send({ status: "File upload not successful", statusNum: 0, errorMsg: error });
     }
 })
 
